61b06791f49413aacbb9dacb89d008b9e37645b3
vishwajeetiitb <vishwajeet724728@gmail.com>
2020-01-03 21:29:20 +0530

    0 val of coordinates removed

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
       // prompted by your browser. If you see the error "The Geolocation service
       // failed.", it means you probably did not give permission for the browser to
       // locate you.
-var a =0;
-var b =0;
+var a ;
+var b ;
       var map, infoWindow;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
